d8685d64ff3d4bdc4e38449b05a2b7ef8cb4c9eb
Sundareeshwaran <sanjaychili838@gmail.com>
2025-05-01 16:30:18 +0530

    changes

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -1,66 +1,12 @@
-"use client"
-import React, {useEffect, useState} from 'react';
-import { useRouter, useSearchParams } from 'next/navigation';
-import Form from '@components/Form';
+import React, { Suspense } from 'react';
+import EditPrompt from './EditPrompt'; // adjust path if needed
 
-const EditPrompt = () => {
-  const router = useRouter();
-  const searchParams = useSearchParams()
-  const promptId = searchParams.get('id')
-
-  const [submitting, setSubmitting] = useState(false);
-  const [post, setPost] = useState({
-    prompt:"",
-    tag:""
-  });
-   
-  useEffect(()=>{
-    const getPromptDetails = async () => {
-      const response = await fetch(`/api/prompt/${promptId}`)
-      const data = await response.json();
-
-      setPost({
-        prompt: data.prompt,
-        tag: data.tag,
-      })
-    }
-
-    if(promptId) getPromptDetails()
-  },[promptId])
-
-  const updatePrompt = async (e) => {
-    e.preventDefault();
-    setSubmitting(true);
-
-    if(!promptId) return alert('Prompt ID not found.')
-
-    try {
-      const response = await fetch(`/api/prompt/${promptId}`,{
-        method: "PATCH",
-        body: JSON.stringify({
-          prompt: post.prompt,
-          tag: post.tag
-        })
-      })
-      if(response.ok) {
-        router.push("/")
-      }
-    } catch (error) {
-      console.log(error)
-    } finally {
-      setSubmitting(false)
-    }
-  }
-
+const Page = () => {
   return (
-    <Form
-    type="Edit"
-    post={post}
-    setPost={setPost}
-    submitting={submitting}
-    handleSubmit={updatePrompt}
-    />
-  )
-}
+    <Suspense fallback={<div>Loading...</div>}>
+      <EditPrompt />
+    </Suspense>
+  );
+};
 
-export default EditPrompt
+export default Page;
