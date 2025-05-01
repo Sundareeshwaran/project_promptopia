b160a526caa16dc3e94378799405fa99fabc15f2
Sundareeshwaran <sanjaychili838@gmail.com>
2025-05-01 17:07:25 +0530

    profile changes

--- components/Profile.jsx
+++ components/Profile.jsx
@@ -4,7 +4,7 @@ import PromptCard from './PromptCard';
 const Profile = ({name, desc, data, handleEdit, handleDelete}) => {
   return (
     <section className='w-full'>
-      <h1 className='head_text text-left'>
+      <h1 className='head_text text-left overflow-clip'>
         <span className='blue_gradient'>{name} Profile</span>
       </h1>
       <p className='desc text-left'>{desc}</p>
